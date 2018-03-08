23d719f5bee2c8ac80d7be2a567bc7387ebfcbcd
Johnny <jboden264@gmail.com>
2018-03-08 12:40:01 -0500

    Worked on nav

--- includes/myscripts.js
+++ includes/myscripts.js
@@ -11,11 +11,11 @@ function MobileDropFunc() {
     var x = document.getElementById("MobileNavAcc");
     if (x.className.indexOf("w3-show") == -1) {
       x.className += " w3-show";
-      x.previousElementSibling.className += " w3-green";
+      x.previousElementSibling.className += " w3-brown";
     } else {
       x.className = x.className.replace(" w3-show", "");
       x.previousElementSibling.className =
-        x.previousElementSibling.className.replace(" w3-green", "");
+        x.previousElementSibling.className.replace(" w3-brown", "");
     }
   }
   function myMap() {

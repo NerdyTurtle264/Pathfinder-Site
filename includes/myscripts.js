d59be6366f4446b38c13ab8baa5f0abfe67f3135
Johnny Boden <jboden264@gmail.com>
2018-03-22 12:41:36 -0400

    Moved header icons

--- includes/myscripts.js
+++ includes/myscripts.js
@@ -11,11 +11,10 @@ function MobileDropFunc() {
     var x = document.getElementById("MobileNavAcc");
     if (x.className.indexOf("w3-show") == -1) {
       x.className += " w3-show";
-      x.previousElementSibling.className += " w3-green";
+      x.previousElementSibling.style = "background-color:gold; color:black";
     } else {
       x.className = x.className.replace(" w3-show", "");
-      x.previousElementSibling.className =
-        x.previousElementSibling.className.replace(" w3-green", "");
+      x.previousElementSibling.style = "background-color:maroon; color:white";
     }
   }
   function myMap() {

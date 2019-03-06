ed09dd5a0424ab4c4374bbd2567a4b0b2a6d8eb9
Johnny <jboden264@gmail.com>
2019-03-06 17:29:56 -0500

    Started BattleArena

---
+++ includes/BattleArena.js
@@ -0,0 +1,5 @@
+var attack = {atkBonus:0, dmgBonus:0, dmgDiceType:6, dmgDiceNum:1, addDmgDiceType:6, addDmgDiceNum:0}
+var combatant = {hp:0, dr:0, ac:10, touch:10, init:0, numAttacks:1, attacks:[], critRange:20, vsTouch:false, addCritDmgType:6, addCritDmgDice:1, critConfirmBonus:0, sneakDamage:0}
+
+window.onload = function () {
+};
